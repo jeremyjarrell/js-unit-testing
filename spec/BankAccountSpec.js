066dae80d58fc7a5a20a3403eb2e83698ecb5fae
Jeremy Jarrell <jjarrell@listhub.com>
2013-06-01 15:50:58 -0400

    Adds the ability to accept deposits

--- spec/BankAccountSpec.js
+++ spec/BankAccountSpec.js
@@ -6,3 +6,14 @@ describe("When opening a bank account", function() {
 		expect(account.balance).toBe(10);
 	});
 });
+
+describe("When depositing funds into a bank account", function() {
+
+	it("should credit that amount against the balance", function() {
+
+		var account = new BankAccount(10);
+		account.deposit(5);
+
+		expect(account.balance).toBe(15);
+	});
+});
--- src/BankAccount.js
+++ src/BankAccount.js
@@ -1,3 +1,7 @@
 function BankAccount (amount) {
 	this.balance = amount;
 }
+
+BankAccount.prototype.deposit = function(amount) {
+	this.balance += amount;
+};
