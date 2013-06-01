fcd062bd1d5c2b8106cd056ef527a2376ab5e3f9
Jeremy Jarrell <jjarrell@listhub.com>
2013-06-01 15:55:52 -0400

    Adds the ability to withdraw funds from my account up to my current balance.

--- spec/BankAccountSpec.js
+++ spec/BankAccountSpec.js
@@ -17,3 +17,22 @@ describe("When depositing funds into a bank account", function() {
 		expect(account.balance).toBe(15);
 	});
 });
+
+describe("When withdrawing funds from a bank account", function() {
+
+	it("should deduct that amount from my balance", function() {
+
+		var account = new BankAccount(10);
+		account.withdraw(7);
+
+		expect(account.balance).toBe(3);
+	});
+
+	it("should throw an error when I try to withdraw an amount more than my balance", function() {
+
+		var account = new BankAccount(10);
+		
+
+		expect(function() { account.withdraw(15); }).toThrow(new Error("Insufficient Funds"));
+	});
+});
--- src/BankAccount.js
+++ src/BankAccount.js
@@ -5,3 +5,10 @@ function BankAccount (amount) {
 BankAccount.prototype.deposit = function(amount) {
 	this.balance += amount;
 };
+
+BankAccount.prototype.withdraw = function(amount) {
+	if (amount > this.balance)
+		throw new Error("Insufficient Funds");
+
+	this.balance -= amount;
+};
